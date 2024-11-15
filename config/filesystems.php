e7ee42b8677f7a2fb386e2b3db75aee1ca1bc587
Vitaliy Lyubimov <lubimov@ukr.net>
2024-11-15 14:29:43 +0100

    Updated DB config to use redis auth

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,7 @@ return [
             'disk'   => 's3-common',
             'prefix' => env('TENANT_ID') ?: env('APPLICATION_ID'),
         ],
+
         's3-common' => [
             'driver'   => 's3',
             'key'      => env('AWS_ACCESS_KEY_ID'),
